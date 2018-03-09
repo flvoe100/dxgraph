76bff031afcdcf2c89af99e09c5373c555f78376
Philipp Helo Rehs <Philipp.Rehs@uni-duesseldorf.de>
2018-03-09 20:01:40 +0100

    update build script for jni nauty

--- jni/build_nauty_lib.sh
+++ jni/build_nauty_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -g -O2 -shared -fpic -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -I"../deps/nauty/" -L"../deps/nauty/" ../deps/nauty/naugraph.c ../deps/nauty/nauty.c ../deps/nauty/showg.c ../deps/nauty/nautil.c JNINauty.c -E
+#gcc -g -O2 -shared -fpic -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -I"../deps/nauty/" -L"../deps/nauty/" ../deps/nauty/naugraph.c ../deps/nauty/nauty.c ../deps/nauty/showg.c ../deps/nauty/nautil.c JNINauty.c -E
 gcc -g -O2 -shared -fpic -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -I"../deps/nauty/" -L"../deps/nauty/" ../deps/nauty/naugraph.c ../deps/nauty/nauty.c ../deps/nauty/showg.c ../deps/nauty/nautil.c JNINauty.c -Wl,-soname,libJNINauty.so -o libJNINauty.so
 #gcc -O0 -shared -fpic -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -I"../nauty/" -L"../nauty/" -o libNautyCheck.so de_hhu_bsinfo_dxgraph_algo_isomorphy_JNINauty.c
 #gcc -Wl,--add-stdcall-alias -shared -o libNautyCheck.so de_hhu_bsinfo_dxgraph_algo_isomorphy_JNINauty.o
